54677f4c5b88dada14ea8a4f56e3c5e28b7a7c50
Yura Pon <yra.pank@gmail.com>
2018-09-25 22:57:11 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count=0;
+  if( number<= 10^8 && number>=1){
+    let num = number;
+    for(let i=5;i<=number;i*=5){
+        count+=Math.floor(num /i);  
+    }
+  }
+  return count;
 }
